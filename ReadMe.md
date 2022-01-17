47dcd2d0ec003c36d167857ccefb69358fb6d437
Lvv.me <cntrump@gmail.com>
2022-01-17 18:34:59 +0800

    init.

---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+# Build 7-Zip for macOS
+
+macOS version min: 10.13
+
+Supported archs: `arm64`, `x86_64`
+
+## How to build
+
+```
+./build.sh
+```
+
+Or
+
+```
+./build.sh 21.07
+```
---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env zsh
+
+set -e
+
+version=$1
+if [ "$version" = "" ]; then
+  version=21.07
+fi
+
+# Remove all dots
+ver=${version//./}
+
+curl -L https://sourceforge.net/projects/sevenzip/files/7-Zip/${version}/7z${ver}-src.tar.xz/download \
+     --output 7z${ver}-src.tar.xz
+
+tar xvf 7z${ver}-src.tar.xz
+
+if [ -d 7z${ver}-src ]; then
+  pushd 7z${ver}-src
+else
+  pushd .
+fi
+
+cd CPP/7zip/Bundles/Alone2
+
+export SDKROOT=`xcrun --sdk macosx --show-sdk-path`
+export MACOSX_DEPLOYMENT_TARGET=10.13
+
+make -j -f ../../cmpl_mac_arm64.mak
+make -j -f ../../cmpl_mac_x64.mak
+
+lipo -create b/m_arm64/7zz b/m_x64/7zz -output b/7zz
+lipo -archs b/7zz
+otool -L b/7zz
+
+b/7zz --help
+
+popd
+
